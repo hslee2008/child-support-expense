0e57763aaa4631f0c28ee38efe6cb5148d4526a3
Hyunseung <hyunseunglee2008@gmail.com>
2023-11-25 18:45:02 +0900

    v0.0.2

---
+++ src/pages/cse-explanation.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Button } from "flowbite-svelte";
+
+</script>
+
+<div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/3" style="width: 100vw">
+  <img src="/양육비산정기준표-1.png" alt="양육비산정기준표-1" />
+  <img src="/양육비산정기준표-2.png" alt="양육비산정기준표-2" />
+
+  <Button color="light" href="/" class="mt-3 mb-7 w-72">홈으로</Button>
+</div>
+
---
+++ src/pages/cse.svelte
@@ -0,0 +1,172 @@
+<script>
+  import { GoogleAnalytics } from "@beyonk/svelte-google-analytics";
+  import {
+    Button,
+    GradientButton,
+    ButtonGroup,
+    InputAddon,
+    Input,
+    Card,
+    Table,
+    TableBody,
+    TableBodyCell,
+    TableBodyRow,
+    TableHead,
+    TableHeadCell,
+    Footer,
+  } from "flowbite-svelte";
+  import { LinkOutline } from "flowbite-svelte-icons";
+
+  import { toReadableMoney } from "../utils/number";
+  import { calculateAge } from "../utils/date";
+
+  import { table, ageKey, getIncomeKey } from "../data/child-support-expanse";
+
+  let today = new Date();
+  let todayString = `${today.getFullYear()}-${
+    today.getMonth() + 1
+  }-${today.getDate()}`;
+  let todayMinus19 = `${today.getFullYear() - 19}-${
+    today.getMonth() + 1
+  }-${today.getDate()}`;
+
+  let birthDate, age;
+  let birthDateString = todayString;
+
+  let parentIncome = {
+    father: localStorage.getItem("father_income") ?? "",
+    mother: localStorage.getItem("mother_income") ?? "",
+  };
+  let parentIncomeTotal = 0;
+
+  $: {
+    birthDate = new Date(birthDateString);
+    age = calculateAge(birthDate);
+  }
+
+  $: {
+    localStorage.setItem("father_income", parentIncome.father);
+    localStorage.setItem("mother_income", parentIncome.mother);
+
+    parentIncomeTotal =
+      Number(parentIncome.father) + Number(parentIncome.mother);
+  }
+
+  let tableKey = `${ageKey[age]},${getIncomeKey(parentIncomeTotal)}`;
+  let resultIncome = 0;
+
+  $: {
+    tableKey = `${ageKey[age]}/${getIncomeKey(parentIncomeTotal)}`;
+
+    try {
+      resultIncome = table[tableKey.split("/")[0]][tableKey.split("/")[1]];
+    } catch (e) {
+      resultIncome = 0;
+    }
+  }
+
+  let daddy = 0;
+  let mommy = 0;
+
+  $: {
+    if (resultIncome) {
+      daddy =
+        parseInt(resultIncome.replaceAll(",", "")) *
+        (parseInt(parentIncome.father) /
+          (parseInt(parentIncome.father) + parseInt(parentIncome.mother)));
+      mommy =
+        parseInt(resultIncome.replaceAll(",", "")) *
+        (parseInt(parentIncome.mother) /
+          (parseInt(parentIncome.father) + parseInt(parentIncome.mother)));
+    }
+  }
+</script>
+
+<GoogleAnalytics properties={["G-7ZHKHQVKNH"]} />
+
+<p>양육비 산정기준표</p>
+<h1>자녀 양육비 계산기</h1>
+
+<Card class="card" style="margin: auto; margin-top: 20px;">
+  <div class="mb-7">
+    <p>자녀 출생년도</p>
+    <input
+      type="date"
+      bind:value={birthDateString}
+      min={todayMinus19}
+      max={todayString}
+      step="1"
+    />
+  </div>
+
+  <p>부모 소득 (단위: 만 원, 세전)</p>
+  <div>
+    <ButtonGroup class="w-full mb-3">
+      <InputAddon>부</InputAddon>
+      <Input
+        type="number"
+        placeholder="예) 100"
+        bind:value={parentIncome.father}
+        style="border-radius: 0px !important;"
+      />
+    </ButtonGroup>
+    <ButtonGroup class="w-full">
+      <InputAddon>모</InputAddon>
+      <Input
+        type="number"
+        placeholder="예) 100"
+        bind:value={parentIncome.mother}
+        style="border-radius: 0px !important;"
+      />
+    </ButtonGroup>
+    <p class="mt-2 text-justify text-sm">
+      소득은 세전소득으로 근로소득, 사업소득, 부동산임대소득, 이자수입, 정부보조금, 연금 등을 모두 합산한 순수입의 총액
+    </p>
+  </div>
+</Card>
+
+<Card class="card" style="margin: auto; margin-top: 20px;">
+  <p>자녀 양육비</p>
+
+  {#if parentIncome.father && parentIncome.mother}
+    <h1>{resultIncome}원</h1>
+
+    <Table class="mt-3 text-center" shadow>
+      <TableHead>
+        <TableHeadCell>부 부담액</TableHeadCell>
+        <TableHeadCell>모 부담액</TableHeadCell>
+      </TableHead>
+      <TableBody>
+        <TableBodyRow>
+          <TableBodyCell>
+            {toReadableMoney(daddy)}
+          </TableBodyCell>
+          <TableBodyCell>
+            {toReadableMoney(mommy)}
+          </TableBodyCell>
+        </TableBodyRow>
+      </TableBody>
+    </Table>
+  {/if}
+</Card>
+
+<Footer class="w-full mt-12 pb-3">
+  <GradientButton
+    color="cyan"
+    href="https://slfamily.scourt.go.kr/dcboard/new/DcNewsViewAction.work?seqnum=9654&gubun=41"
+  >
+    <LinkOutline class="mr-2" /> 서울가정법원 양육비 산정기준표 안내
+  </GradientButton>
+
+  <br />
+
+  <Button color="light" href="/" class="mt-2">홈으로</Button>
+</Footer>
+
+<style>
+  @media (max-width: 768px) {
+    h1 {
+      font-size: 2rem;
+    }
+  }
+</style>
