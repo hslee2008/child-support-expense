bf81b8bebf61d36a1156a4efe9d0dbce5449c8cb
Hyunseung <hyunseunglee2008@gmail.com>
2023-11-25 19:55:13 +0900

    setup bubblewrap

---
+++ android/app/src/main/java/app/web/child_support_expanse/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.web.child_support_expanse.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
