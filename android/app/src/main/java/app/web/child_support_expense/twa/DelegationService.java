428f05239402d80c9c3070c81d2ddd9caf2e3e50
Hyunseung <hyunseunglee2008@gmail.com>
2023-11-26 17:19:55 +0900

    rewrite using sveltekit

---
+++ android/app/src/main/java/app/web/child_support_expense/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.web.child_support_expense.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
