a2023d60c59adacec3c36584c79f3b7a906f4ffa
Hyunseung <hyunseunglee2008@gmail.com>
2023-11-26 17:53:07 +0900

    sveltekit - add google ananlytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script>
-import "../app.pcss";
-import "../app.pcss";
+	import '../app.pcss';
+
+	import { GoogleAnalytics } from '@beyonk/svelte-google-analytics';
 </script>
 
 <slot />
+
+<GoogleAnalytics properties={['G-BL6Z0JDBB1']} />
