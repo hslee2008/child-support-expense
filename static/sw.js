3505f7072971a27b9c8cbb354c4755e1453d8b4c
Hyunseung <hyunseunglee2008@gmail.com>
2023-11-29 22:09:49 +0900

    add heavy png images to caching

--- static/sw.js
+++ static/sw.js
@@ -1,51 +1,51 @@
-"use strict";
+'use strict';
 
-const CACHE_NAME = "child-support-expense-v3";
+const CACHE_NAME = 'child-support-expense-v3';
 
 const FILES_TO_CACHE = [
-  "/images/48x48.png",
-  "/images/72x72.png",
-  "/images/96x96.png",
-  "/images/144x144.png",
-  "/images/192x192.png",
-  "/images/512x512.png",
-  "/images/maskable.png",
-  "/icon.png",
-  "/favicon.ico",
-  "manifest.json"
+	'/images/48x48.png',
+	'/images/72x72.png',
+	'/images/96x96.png',
+	'/images/144x144.png',
+	'/images/192x192.png',
+	'/images/512x512.png',
+	'/images/maskable.png',
+	'/icon.png',
+	'/favicon.ico',
+	'manifest.json',
+	'양육비산정기준표-1.png',
+	'양육비산정기준표-2.png'
 ];
 
-self.addEventListener("install", (evt) => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+self.addEventListener('install', (evt) => {
+	evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+	self.skipWaiting();
 });
 
-self.addEventListener("activate", (evt) => {
-  // Remove previous cached data from disk.
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', (evt) => {
+	// Remove previous cached data from disk.
+	evt.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (key !== CACHE_NAME) {
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
 
-  self.clients.claim();
+	self.clients.claim();
 });
 
-self.addEventListener("fetch", (evt) => {
-  if (evt.request.mode !== "navigate") return;
+self.addEventListener('fetch', (evt) => {
+	if (evt.request.mode !== 'navigate') return;
 
-  evt.respondWith(
-    fetch(evt.request).catch(async () => {
-      const cache = await caches.open(CACHE_NAME);
-      return await cache.match("offline.html");
-    })
-  );
+	evt.respondWith(
+		fetch(evt.request).catch(async () => {
+			const cache = await caches.open(CACHE_NAME);
+			return await cache.match('offline.html');
+		})
+	);
 });
